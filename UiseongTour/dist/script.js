8a5d656701bae558c2cc2754faccb27c9e3211d0
chieyoun <mundy66@naver.com>
2022-12-08 17:12:33 +0900

    sdf

--- UiseongTour/dist/script.js
+++ UiseongTour/dist/script.js
@@ -8,15 +8,16 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 0 } } },
-
+      gravity: { y: 0 },
+    },
+  },
 
   scene: {
     preload: preload,
     create: create,
-    update: update } };
-
-
+    update: update,
+  },
+};
 
 const game = new Phaser.Game(config);
 let cursors;
@@ -25,20 +26,32 @@ let showDebug = false;
 
 function preload() {
   //타일셋 이미지
-  this.load.image("tiles", "https://mikewesthad.github.io/phaser-3-tilemap-blog-posts/post-1/assets/tilesets/tuxmon-sample-32px-extruded.png");
+  this.load.image(
+    "tiles",
+    "../asset/img/tiled/5Camp.png"
+    //"https://mikewesthad.github.io/phaser-3-tilemap-blog-posts/post-1/assets/tilesets/tuxmon-sample-32px-extruded.png"
+  );
   //타일셋 위치 정하는 json 파일
-  this.load.tilemapTiledJSON("map", "https://mikewesthad.github.io/phaser-3-tilemap-blog-posts/post-1/assets/tilemaps/tuxemon-town.json");
+  this.load.tilemapTiledJSON(
+    "map",
+    "https://mikewesthad.github.io/phaser-3-tilemap-blog-posts/post-1/assets/tilemaps/tuxemon-town.json"
+    //"../asset/json/untitled.json"
+  );
 
   // 아틀라스: 여러 이미지를 하나의 텍스처로 묶는 방법
 
   // 하나의 이미지에서 플레이어 애니메이션(왼쪽으로 걷기, 오른쪽으로 걷기 등).
   //      참조링크: https://labs.phaser.io/view.html?src=src/animation/texture%20atlas%20animation.js
-  
+
   // 아틀라스를 사용하지 않는 경우 스프라이트시트로 동일한 작업을 수행 가능.
   //      참조링크:  https://labs.phaser.io/view.html?src=src/animation/single%20sprite%20sheet.js
-  
+
   //캐릭터 아틀라스
-  this.load.atlas("atlas", "https://mikewesthad.github.io/phaser-3-tilemap-blog-posts/post-1/assets/atlas/atlas.png", "https://mikewesthad.github.io/phaser-3-tilemap-blog-posts/post-1/assets/atlas/atlas.json");
+  this.load.atlas(
+    "atlas",
+    "../asset/img/char/player.png",
+    "https://mikewesthad.github.io/phaser-3-tilemap-blog-posts/post-1/assets/atlas/atlas.json"
+  );
 }
 
 function create() {
@@ -62,15 +75,18 @@ function create() {
 
   // Tiled의 객체 레이어를 사용하면 생성 지점이나 사용자 지정과 같은 추가 정보를 지도에 포함 가능.
   // 충돌 모양. tmx 파일에는 "Spawn Point"라는 이름의 점이 있는 개체 레이어 존재.
-  const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
+  const spawnPoint = map.findObject(
+    "Objects",
+    (obj) => obj.name === "Spawn Point"
+  );
 
   // 물리 시스템을 통해 물리가 활성화된 스프라이트를 만듬.
   // 스프라이트에 사용된 이미지는 약간의 공백이 있으므로 setSize 및 setOffset을 사용하여 플레이어의 몸 크기를 제어.
-  
-  player = this.physics.add.
-  sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front").
-  setSize(30, 40).
-  setOffset(0, 24);
+
+  player = this.physics.add
+    .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
+    .setSize(30, 40)
+    .setOffset(0, 24);
 
   // 장면이 지속되는 동안 플레이어와 worldLayer가 충돌하는지 확인.
   this.physics.add.collider(player, worldLayer);
@@ -80,28 +96,51 @@ function create() {
   const anims = this.anims;
   anims.create({
     key: "misa-left-walk",
-    frames: anims.generateFrameNames("atlas", { prefix: "misa-left-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-left-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3,
+    }),
     frameRate: 10,
-    repeat: -1 });
+    repeat: -1,
+  });
 
   anims.create({
     key: "misa-right-walk",
-    frames: anims.generateFrameNames("atlas", { prefix: "misa-right-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-right-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3,
+    }),
     frameRate: 10,
-    repeat: -1 });
+    repeat: -1,
+  });
 
   anims.create({
     key: "misa-front-walk",
-    frames: anims.generateFrameNames("atlas", { prefix: "misa-front-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-front-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3,
+    }),
     frameRate: 10,
-    repeat: -1 });
+    repeat: -1,
+  });
 
   anims.create({
     key: "misa-back-walk",
-    frames: anims.generateFrameNames("atlas", { prefix: "misa-back-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-back-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3,
+    }),
     frameRate: 10,
-    repeat: -1 });
-
+    repeat: -1,
+  });
 
   const camera = this.cameras.main;
   camera.startFollow(player);
@@ -110,30 +149,27 @@ function create() {
   cursors = this.input.keyboard.createCursorKeys();
 
   // 화면에서 "fixed" 위치에 있는 도움말 텍스트 (하얀박스 안의 글자)
-  this.add.
-  text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
-    font: "18px monospace",
-    fill: "#000000",
-    padding: { x: 20, y: 10 },
-    backgroundColor: "#ffffff" }).
-
-  setScrollFactor(0).
-  setDepth(30);
+  this.add
+    .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
+      font: "18px monospace",
+      fill: "#000000",
+      padding: { x: 20, y: 10 },
+      backgroundColor: "#ffffff",
+    })
+    .setScrollFactor(0)
+    .setDepth(30);
 
   // Debug graphics
-  this.input.keyboard.once("keydown-D", event => {
+  this.input.keyboard.once("keydown-D", (event) => {
     // 플레이어의 히트박스를 표시하려면 물리 디버깅을 켜기.
     this.physics.world.createDebugGraphic();
 
     // 플레이어 위에 있지만 도움말 텍스트 아래에는 worldLayer 충돌 그래픽을 만듬.
-    const graphics = this.add.
-    graphics().
-    setAlpha(0.75).
-    setDepth(20);
+    const graphics = this.add.graphics().setAlpha(0.75).setDepth(20);
     worldLayer.renderDebug(graphics, {
       tileColor: null, //충돌되지 않는 타일의 색
       collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // 충돌되는 타일의 색
-      faceColor: new Phaser.Display.Color(40, 39, 37, 255) // 충돌되는 면 가장자리의 색
+      faceColor: new Phaser.Display.Color(40, 39, 37, 255), // 충돌되는 면 가장자리의 색
     });
   });
 }
@@ -175,9 +211,9 @@ function update(time, delta) {
     player.anims.stop();
 
     // 우리가 움직이고 있다면 사용할 프레임을 선택하고 사용되지 않는 상태로 만듬.
-    if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");else
-    if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");else
-    if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");else
-    if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
+    if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
+    else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
+    else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
+    else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
   }
 }

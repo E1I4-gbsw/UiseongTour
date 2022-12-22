92e74164f5e44b5240bb8ef97033afb27994b279
chieyoun <mundy66@naver.com>
2022-12-22 17:09:32 +0900

    hjk

--- UiseongTour/asset/img/tiled/5Camp.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="5ddd" tilewidth="32" tileheight="32" tilecount="5408" columns="32">
- <image source="D:/workspace/asset/Modern_Exteriors_v15.4/Modern_Exteriors_32x32/ME_Theme_Sorter_32x32/5_Camping_32x32.png" width="1024" height="5408"/>
-</tileset>
